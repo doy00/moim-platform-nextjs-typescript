73a77b7dc3a06d659eca1d082647e5b664e072e3
ayoooyh <syh9566@naver.com>
2025-01-02 11:04:15 +0900

    design : 대략적인 위치 설정

---
+++ src/app/mypage/meetings/page.tsx
@@ -0,0 +1,11 @@
+export default function Meetings() {
+  return (
+    <div>
+      {/* TODO: 카테고리 부분 나의 모임, 나의 리뷰, 내가 만든 모임 병합은 각 페이지 구현 후 mypage 브랜치에
+        서 병합하기 */}
+      <div>
+        <span>나의 모임</span>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,25 @@
+export default function Mypage() {
+  return (
+    <div className="h-screen max-w-[1200px] mx-auto">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-2xl font-bold ">마이페이지</h1>
+        {/* 내 프로필 구간 */}
+        <div className="flex flex-col border-2 border-gray-300 rounded-md p-4 gap-4">
+          <div className="flex items-center justify-between ">
+            <h2 className="text-xl font-bold">내 정보</h2>
+            <button className="rounded-full bg-gray-400">프로필 수정</button>
+          </div>
+          <hr />
+          <div className="flex gap-4">
+            <div>profile img</div>
+            <div className="flex flex-col gap-2">
+              <span>닉네임</span>
+              <span>회사</span>
+              <span>이메일</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
