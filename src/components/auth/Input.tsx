736617b9b827536cae81b2a353dabb08710ffda8
Eun Oh <bdohhhhh@gmail.com>
2025-01-02 11:22:56 +0900

    chore: auth / input 컴포넌트 작성

--- src/components/auth/Input.tsx
+++ src/components/auth/Input.tsx
@@ -1,43 +1,44 @@
 'use client';
 
+import { TAuthInputs } from '@/types/auth.type';
 import { cn } from '@/utils/ui.util';
 import * as React from 'react';
+import { UseFormRegister } from 'react-hook-form';
 import Eye from './Eye';
 import EyeSlash from './EyeSlash';
 
-type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  register: UseFormRegister<TAuthInputs>;
+  name: keyof TAuthInputs;
+}
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
-    const [showPassword, setShowPassword] = React.useState(false);
+function Input({ className, type, register, ...props }: InputProps) {
+  const [showPassword, setShowPassword] = React.useState(false);
 
-    const togglePasswordVisibility = () => {
-      setShowPassword((prevShowPassword) => !prevShowPassword);
-    };
-    return (
-      <div className="relative flex items-center w-full">
-        <input
-          type={type === 'password' && showPassword ? 'text' : type}
-          className={cn(
-            'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-            className,
-          )}
-          ref={ref}
-          {...props}
-        />
-        {type === 'password' && (
-          <div
-            className="absolute right-3 cursor-pointer text-muted-foreground"
-            onClick={togglePasswordVisibility}
-          >
-            {showPassword ? <Eye /> : <EyeSlash />}
-          </div>
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+  return (
+    <div className="relative flex items-center w-full">
+      <input
+        type={type === 'password' && showPassword ? 'text' : type}
+        className={cn(
+          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          className,
         )}
-      </div>
-    );
-  },
-);
-
-Input.displayName = 'Input';
+        {...props}
+        {...register(props.name)}
+      />
+      {type === 'password' && (
+        <div
+          className="absolute right-3 cursor-pointer text-muted-foreground"
+          onClick={togglePasswordVisibility}
+        >
+          {showPassword ? <Eye /> : <EyeSlash />}
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default Input;
