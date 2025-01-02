da08ed272cb8d006f66a68e253fc38ffb33facc3
Eun Oh <bdohhhhh@gmail.com>
2025-01-02 14:06:02 +0900

    feat: token 만료시간과 함께 저장 및 읽기 유틸함수 작성

---
+++ src/utils/auth.util.ts
@@ -0,0 +1,19 @@
+export const setItemWithExpireTime = (keyName: string, keyValue: string, tts: number) => {
+  const obj = {
+    value: keyValue,
+    expire: Date.now() + tts,
+  };
+  const objString = JSON.stringify(obj);
+  window.localStorage.setItem(keyName, objString);
+};
+
+export const getItemWithExpireTime = (keyName: string): string | null => {
+  const objString = window.localStorage.getItem(keyName);
+  if (!objString) return null;
+  const obj = JSON.parse(objString);
+  if (Date.now() > obj.expire) {
+    window.localStorage.removeItem(keyName);
+    return null;
+  }
+  return obj.value;
+};
