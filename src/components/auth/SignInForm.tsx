f5ba092272de74d9cd2f4204053588d19805d205
Eun Oh <bdohhhhh@gmail.com>
2025-01-03 14:49:50 +0900

    feat: 회원가입, 로그인 에러상황 예외 처리

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -2,76 +2,103 @@
 
 import { useSignInMutation } from '@/hooks/auth.hook';
 import { TAuthInputs } from '@/types/auth.type';
+import { useDebounce } from '@/utils/auth.util';
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import Input from './Input';
 
 export default function SignInForm() {
+  const router = useRouter();
   const {
     register,
     handleSubmit,
+    trigger,
+    setValue,
     formState: { errors },
-  } = useForm<TAuthInputs>();
-  const { mutate: signIn, isPending: isSignInPending, error: signInError } = useSignInMutation();
+  } = useForm<TAuthInputs>({
+    mode: 'onBlur',
+  });
+  const {
+    mutateAsync: signIn,
+    isPending: isSignInPending,
+    error: signInError,
+    reset,
+  } = useSignInMutation();
 
-  const onSubmit = (data: TAuthInputs) => {
-    signIn(data);
+  const debouncedValidation = useDebounce((name: keyof TAuthInputs) => {
+    trigger(name);
+  }, 1000);
+
+  const onSubmit = async (data: TAuthInputs) => {
+    const response = await signIn(data);
+    if (response.token) router.push('/');
   };
 
   useEffect(() => {
-    if (signInError) {
-      console.error(signInError);
-    }
+    if (signInError) console.error(signInError);
   }, [signInError]);
 
-  // TODO: 로딩 상태 표시
-  if (isSignInPending) {
-    return <div>로딩 중...</div>;
-  }
-
   return (
-    <>
-      <div className="flex flex-col items-center justify-center gap-2 pb-6">
-        <h1 className="text-title-1 font-bold">회원가입</h1>
-      </div>
-
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="w-full h-fit flex flex-col items-center justify-center gap-10"
-      >
-        <div className="w-[90%] flex flex-col items-center justify-center gap-10">
-          <div className="w-full flex flex-col gap-4">
-            <div className="flex flex-col gap-2">
-              <Input
-                type="text"
-                placeholder="이메일을 입력해주세요"
-                name="email"
-                register={register('email', { required: '이메일을 입력해주세요' })}
-              />
-              {errors.email && <p className="text-red-500">{errors.email.message}</p>}
-            </div>
-            <div className="flex flex-col gap-2">
-              <Input
-                type="password"
-                placeholder="비밀번호를 입력해주세요"
-                name="password"
-                register={register('password', {
-                  required: '비밀번호를 입력해주세요',
-                  pattern: {
-                    value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-                    message: '비밀번호는 8자 이상으로 숫자 특수문자 포함해주세요',
-                  },
-                })}
-              />
-              {errors.password && <p className="text-red-500">{errors.password.message}</p>}
-            </div>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-full h-fit flex flex-col items-center justify-center gap-10"
+    >
+      {isSignInPending && <div>로딩 중...</div>}
+      <div className="w-[90%] flex flex-col items-center justify-center gap-10">
+        <div className="w-full flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <Input
+              type="text"
+              placeholder="이메일을 입력해주세요"
+              name="email"
+              register={register('email', {
+                required: '이메일을 입력해주세요',
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: '올바른 이메일 형식이 아닙니다.',
+                },
+                onChange: (e) => {
+                  if (signInError) reset();
+                  setValue('email', e.target.value);
+                  debouncedValidation('email');
+                },
+              })}
+            />
+            {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+            {signInError && signInError.code === 'USER_NOT_FOUND' && (
+              <p className="text-red-500">{signInError.message}</p>
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            <Input
+              type="password"
+              placeholder="비밀번호를 입력해주세요"
+              name="password"
+              register={register('password', {
+                required: '비밀번호를 입력해주세요',
+                minLength: {
+                  value: 8,
+                  message: '비밀번호는 최소 8자 이상이어야 합니다.',
+                },
+                onChange: (e) => {
+                  if (signInError) reset();
+                  setValue('password', e.target.value);
+                  debouncedValidation('password');
+                },
+              })}
+            />
+            {errors.password && <p className="text-red-500">{errors.password.message}</p>}
+            {signInError && signInError.code === 'INVALID_CREDENTIALS' && (
+              <p className="text-red-500">{signInError.message}</p>
+            )}
           </div>
         </div>
+      </div>
 
-        <button className="bg-gray800 text-white rounded-lg px-4 py-2" type="submit">
-          회원가입
-        </button>
-      </form>
-    </>
+      <button className="bg-gray800 text-white rounded-lg px-4 py-2" type="submit">
+        로그인
+      </button>
+    </form>
   );
 }
--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -3,11 +3,13 @@
 import { useSignUpMutation } from '@/hooks/auth.hook';
 import { TAuthInputs } from '@/types/auth.type';
 import { useDebounce } from '@/utils/auth.util';
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import Input from './Input';
 
 export default function SignUpForm() {
+  const router = useRouter();
   const {
     register,
     handleSubmit,
@@ -18,10 +20,18 @@ export default function SignUpForm() {
   } = useForm<TAuthInputs>({
     mode: 'onBlur',
   });
-  const { mutate: signUp, isPending: isSignUpPending, error: signUpError } = useSignUpMutation();
+  const {
+    mutateAsync: signUp,
+    isPending: isSignUpPending,
+    error: signUpError,
+    reset,
+  } = useSignUpMutation();
 
-  const onSubmit = (data: TAuthInputs) => {
-    signUp(data);
+  const onSubmit = async (data: TAuthInputs) => {
+    if (signUpError && signUpError.message === '이미 사용 중인 이메일입니다') return;
+    delete data.passwordConfirm;
+    const response = await signUp(data);
+    if (response.message === '사용자 생성 성공') router.push('/');
   };
 
   const password = watch('password');
@@ -31,117 +41,118 @@ export default function SignUpForm() {
   }, 1000);
 
   useEffect(() => {
-    if (signUpError) {
-      console.error(signUpError);
-    }
+    if (signUpError) console.error(signUpError);
   }, [signUpError]);
 
-  // TODO: 로딩 상태 표시
-  if (isSignUpPending) {
-    return <div>로딩 중...</div>;
-  }
-
   return (
-    <>
-      <div className="flex flex-col items-center justify-center gap-2 pb-6">
-        <h1 className="text-title-1 font-bold">회원가입</h1>
-      </div>
-
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="w-full h-fit flex flex-col items-center justify-center gap-10"
-      >
-        <div className="w-[90%] flex flex-col items-center justify-center gap-10">
-          <div className="w-full flex flex-col gap-4">
-            <div className="flex flex-col gap-2">
-              <Input
-                type="text"
-                placeholder="이름을 입력해주세요"
-                name="name"
-                register={register('name', {
-                  required: '이름을 입력해주세요',
-                  onChange: (e) => {
-                    setValue('name', e.target.value);
-                    debouncedValidation('name');
-                  },
-                })}
-              />
-              {errors.name && <p className="text-red-500">{errors.name.message}</p>}
-            </div>
-            <div className="flex flex-col gap-2">
-              <Input
-                type="text"
-                placeholder="이메일을 입력해주세요"
-                name="email"
-                register={register('email', {
-                  required: '이메일을 입력해주세요',
-                  onChange: (e) => {
-                    setValue('email', e.target.value);
-                    debouncedValidation('email');
-                  },
-                })}
-              />
-              {errors.email && <p className="text-red-500">{errors.email.message}</p>}
-            </div>
-            <div className="flex flex-col gap-2">
-              <Input
-                type="text"
-                placeholder="회사명을 입력해주세요"
-                name="companyName"
-                register={register('companyName', {
-                  required: '회사명을 입력해주세요',
-                  onChange: (e) => {
-                    setValue('companyName', e.target.value);
-                    debouncedValidation('companyName');
-                  },
-                })}
-              />
-              {errors.companyName && <p className="text-red-500">{errors.companyName.message}</p>}
-            </div>
-            <div className="flex flex-col gap-2">
-              <Input
-                type="password"
-                placeholder="비밀번호를 입력해주세요"
-                name="password"
-                register={register('password', {
-                  required: '비밀번호를 입력해주세요',
-                  onChange: (e) => {
-                    setValue('password', e.target.value);
-                    debouncedValidation('password');
-                  },
-                })}
-              />
-              {errors.password && <p className="text-red-500">{errors.password.message}</p>}
-            </div>
-            <div className="flex flex-col gap-2">
-              <Input
-                type="password"
-                placeholder="비밀번호를 다시 한번 입력해주세요"
-                name="passwordConfirm"
-                register={register('passwordConfirm', {
-                  required: '비밀번호를 다시 한번 입력해주세요',
-                  validate: (value) => {
-                    if (value === password) return true;
-                    return '비밀번호가 일치하지 않습니다.';
-                  },
-                  onChange: (e) => {
-                    console.log('onChange triggered, value:', e.target.value);
-                    setValue('passwordConfirm', e.target.value);
-                    debouncedValidation('passwordConfirm');
-                  },
-                })}
-              />
-              {errors.passwordConfirm && (
-                <p className="text-red-500">{errors.passwordConfirm.message}</p>
-              )}
-            </div>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-full h-fit flex flex-col items-center justify-center gap-10"
+    >
+      {isSignUpPending && <div>로딩 중...</div>}
+      <div className="w-[90%] flex flex-col items-center justify-center gap-10">
+        <div className="w-full flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <Input
+              type="text"
+              placeholder="이름을 입력해주세요"
+              name="name"
+              register={register('name', {
+                required: '이름을 입력해주세요',
+                onChange: (e) => {
+                  if (signUpError) reset();
+                  setValue('name', e.target.value);
+                  debouncedValidation('name');
+                },
+              })}
+            />
+            {errors.name && <p className="text-red-500">{errors.name.message}</p>}
+          </div>
+          <div className="flex flex-col gap-2">
+            <Input
+              type="text"
+              placeholder="이메일을 입력해주세요"
+              name="email"
+              register={register('email', {
+                required: '이메일을 입력해주세요',
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: '올바른 이메일 형식이 아닙니다.',
+                },
+                onChange: (e) => {
+                  if (signUpError) reset();
+                  setValue('email', e.target.value);
+                  debouncedValidation('email');
+                },
+              })}
+            />
+            {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+            {signUpError && <p className="text-red-500">{signUpError.message}</p>}
+          </div>
+          <div className="flex flex-col gap-2">
+            <Input
+              type="text"
+              placeholder="회사명을 입력해주세요"
+              name="companyName"
+              register={register('companyName', {
+                required: '회사명을 입력해주세요',
+                onChange: (e) => {
+                  if (signUpError) reset();
+                  setValue('companyName', e.target.value);
+                  debouncedValidation('companyName');
+                },
+              })}
+            />
+            {errors.companyName && <p className="text-red-500">{errors.companyName.message}</p>}
+          </div>
+          <div className="flex flex-col gap-2">
+            <Input
+              type="password"
+              placeholder="비밀번호를 입력해주세요"
+              name="password"
+              register={register('password', {
+                required: '비밀번호를 입력해주세요',
+                pattern: {
+                  value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+                  message: '비밀번호는 8자 이상으로 숫자 특수문자 포함해주세요',
+                },
+                onChange: (e) => {
+                  if (signUpError) reset();
+                  setValue('password', e.target.value);
+                  debouncedValidation('password');
+                },
+              })}
+            />
+            {errors.password && <p className="text-red-500">{errors.password.message}</p>}
+          </div>
+          <div className="flex flex-col gap-2">
+            <Input
+              type="password"
+              placeholder="비밀번호를 다시 한번 입력해주세요"
+              name="passwordConfirm"
+              register={register('passwordConfirm', {
+                required: '비밀번호를 다시 한번 입력해주세요',
+                validate: (value) => {
+                  if (value === password) return true;
+                  return '비밀번호가 일치하지 않습니다.';
+                },
+                onChange: (e) => {
+                  if (signUpError) reset();
+                  setValue('passwordConfirm', e.target.value);
+                  debouncedValidation('passwordConfirm');
+                },
+              })}
+            />
+            {errors.passwordConfirm && (
+              <p className="text-red-500">{errors.passwordConfirm.message}</p>
+            )}
           </div>
         </div>
+      </div>
 
-        <button className="bg-gray800 text-white rounded-lg px-4 py-2" type="submit">
-          회원가입
-        </button>
-      </form>
-    </>
+      <button className="bg-gray800 text-white rounded-lg px-4 py-2" type="submit">
+        회원가입
+      </button>
+    </form>
   );
 }
