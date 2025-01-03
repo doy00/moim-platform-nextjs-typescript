95e87745a4f2d28c5ebb0c8c0d8ad98d12f6580e
Eun Oh <bdohhhhh@gmail.com>
2025-01-03 14:49:13 +0900

    chore: 오타 수정

--- src/containers/auth/SignInContainer.tsx
+++ src/containers/auth/SignInContainer.tsx
@@ -6,6 +6,9 @@ import Link from 'next/link';
 export default function SignInContainer() {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-2 pb-6">
+        <h1 className="text-title-1 font-bold">로그인</h1>
+      </div>
       <SignInForm />
       <p className="text-sm text-gray-500">
         두더밑이 처음이신가요? <Link href="/signup">회원가입</Link>
--- src/containers/auth/SignUpContainer.tsx
+++ src/containers/auth/SignUpContainer.tsx
@@ -6,6 +6,9 @@ import Link from 'next/link';
 export default function SignUpContainer() {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-2 pb-6">
+        <h1 className="text-title-1 font-bold">회원가입</h1>
+      </div>
       <SignUpForm />
       <p className="text-sm text-gray-500">
         이미 회원이신가요? <Link href="/signin">로그인</Link>
