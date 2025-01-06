a4e5756cbbcdad14aa05a4a5cfb1c4b33369e44f
ayoooyh <syh9566@naver.com>
2025-01-06 09:31:47 +0900

    feat : 내 프로필 api 연결 후 구현

--- src/components/mypage/meetings/Meetings.tsx
+++ src/components/mypage/meetings/Meetings.tsx
@@ -4,7 +4,7 @@ export default function Meetings() {
       {/* TODO: 카테고리 부분 나의 모임, 나의 리뷰, 내가 만든 모임 병합은 각 페이지 구현 후 mypage 브랜치에
         서 병합하기 */}
       <div>
-        <span>나의 모임</span>
+        <h1 className="text-2xl font-bold">나의 모임</h1>
       </div>
     </div>
   );
