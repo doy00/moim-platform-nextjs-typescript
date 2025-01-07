b5ee93ef025a88b1fecd38a7271a4b6fffdee34d
ayoooyh <syh9566@naver.com>
2025-01-08 00:38:58 +0900

    faet : axiosInstance headers 추가

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -5,6 +5,7 @@ const BASE_URL = `https://fe-adv-project-together-dallaem.vercel.app/${TEAM_ID}`
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
+  headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
 });
 
 export default axiosInstance;
