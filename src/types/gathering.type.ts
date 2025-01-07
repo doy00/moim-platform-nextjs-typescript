c161a41fd6d747e9319e13e201f115d480e096b6
ayoooyh <syh9566@naver.com>
2025-01-08 01:10:54 +0900

    feat : 나의 모임 타입 추가

--- src/types/gathering.type.ts
+++ src/types/gathering.type.ts
@@ -3,16 +3,18 @@ export interface IGathering {
   id: number;
   type: string;
   name: string;
-  dateTime: Date;
-  registrationEnd: Date;
+  dateTime: string;
+  registrationEnd: string;
   location: string;
   participantCount: number;
   capacity: number;
   image: string | null;
   createdBy: number;
-  canceledAt: Date | null;
+  canceledAt: string | null;
 }
 
-// export interface IGatheringResponse {
-//   gatherings: IGathering[];
-// }
+export interface IJoind extends IGathering {
+  joinedAt: string;
+  isCompleted: boolean;
+  isReviewed: boolean;
+}
