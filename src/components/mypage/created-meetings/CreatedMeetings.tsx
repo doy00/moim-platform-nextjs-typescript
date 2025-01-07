1e6ef134a18572c8b699d1936a455b4d480b5288
ayoooyh <syh9566@naver.com>
2025-01-07 09:17:49 +0900

    feat : 내가 만든 모임 파트 수정

--- src/components/mypage/created-meetings/CreatedMeetings.tsx
+++ src/components/mypage/created-meetings/CreatedMeetings.tsx
@@ -1,9 +1,29 @@
-import GatheringCard from '@/components/mypage/gatheringCard/GatheringCard';
+'use client';
+
+import GatheringCard from '@/components/mypage/created-meetings/gatheringCard/GatheringCard';
+import { IGathering } from '@/types/gathering.type';
+import { useEffect, useState } from 'react';
+import { getGatherings } from '@/apis/gatherings';
 
 export default function CreatedMeetings() {
+  const [gatherings, setGatherings] = useState<IGathering[]>([]);
+  // 임시 userId
+  const userId = 1006;
+
+  useEffect(() => {
+    const fetchGatherings = async () => {
+      const data = await getGatherings();
+      const filteredData = data.filter((gathering) => gathering.createdBy === userId);
+      setGatherings(filteredData);
+    };
+    fetchGatherings();
+  }, [userId]);
+
   return (
     <div>
-      <GatheringCard />
+      {gatherings.map((gathering) => (
+        <GatheringCard key={gathering.id} gathering={gathering} />
+      ))}
     </div>
   );
 }
