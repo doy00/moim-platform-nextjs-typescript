35cee0c4af48968372bd470cc95f09fb8e791104
ayoooyh <syh9566@naver.com>
2025-01-10 13:07:54 +0900

    feat : 쿼리 적용

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,15 +1,28 @@
-'use client';
-
+import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
+import { getQueryClient } from '@/hooks/mypage/queries/getQueryClient';
+import { getJoined } from '@/apis/getJoined';
 import Header from '@/components/mypage/header/Header';
 import MyProfile from '@/components/mypage/myprofile/MyProfile';
 import RenderTab from '@/components/mypage/renderTab/RenderTab';
 
-export default function Mypage() {
+export default async function Mypage() {
+  const queryClient = getQueryClient();
+
+  // 서버에서 미리 데이터 페칭
+  await queryClient.prefetchQuery({
+    queryKey: ['gatherings'],
+    queryFn: getJoined,
+  });
+
+  const dehydratedState = dehydrate(queryClient);
+
   return (
-    <div className="h-screen max-w-[1200px] flex flex-col gap-4">
-      <Header />
-      <MyProfile />
-      <RenderTab />
-    </div>
+    <HydrationBoundary state={dehydratedState}>
+      <div className="h-screen max-w-[1200px] flex flex-col gap-4">
+        <Header />
+        <MyProfile />
+        <RenderTab />
+      </div>
+    </HydrationBoundary>
   );
 }
