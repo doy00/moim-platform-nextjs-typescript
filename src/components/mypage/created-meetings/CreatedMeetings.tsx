6c840b841348ac57705e0fbed4628107f99f1a79
ayoooyh <syh9566@naver.com>
2025-01-10 14:19:47 +0900

    feat : 모임 개설 버튼 모션 추가

--- src/components/mypage/created-meetings/CreatedMeetings.tsx
+++ src/components/mypage/created-meetings/CreatedMeetings.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import emptyDudu from '../../../../public/images/mypage/dudu-empty.svg';
 import Link from 'next/link';
 import { useCreatedGatheringsQuery } from '@/hooks/mypage/queries/useGatheringsQuery';
+import { motion } from 'framer-motion';
 
 export default function CreatedMeetings() {
   const { data, isLoading } = useCreatedGatheringsQuery();
@@ -20,9 +21,15 @@ export default function CreatedMeetings() {
           <Image src={emptyDudu} alt="empty" width={180} height={180} />
           <p className="text-body-2-reading text-gray300">아직 만든 모임이 없어요</p>
         </div>
-        <Link href="/gatherings" className="rounded-xl bg-gray950 px-5 py-2">
-          <span className="font-semibold text-label-normal text-gray50">모임 개설하기</span>
-        </Link>
+        <motion.div
+          whileHover={{ scale: 1.2 }}
+          whileTap={{ scale: 0.9 }}
+          transition={{ type: 'spring', stiffness: 400, damping: 17 }}
+        >
+          <Link href="/gatherings" className="rounded-xl bg-gray950 px-5 py-2">
+            <span className="font-semibold text-label-normal text-gray50">모임 개설하기</span>
+          </Link>
+        </motion.div>
       </div>
     );
   }
