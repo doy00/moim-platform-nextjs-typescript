c3f5bff59cdfc7386f976ba9b596c3eacdf933c2
ayoooyh <syh9566@naver.com>
2025-01-10 16:08:59 +0900

    feat : 모션으로 수정

--- src/components/mypage/filterBar/switch/switch.tsx
+++ src/components/mypage/filterBar/switch/switch.tsx
@@ -1,29 +1,32 @@
-'use client';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
 
-import * as React from 'react';
-import * as SwitchPrimitives from '@radix-ui/react-switch';
+export default function Switch2() {
+  const [isOn, setIsOn] = useState(false);
 
-import { cn } from '@/lib/utils';
+  const toggleSwitch = () => setIsOn(!isOn);
 
-const Switch = React.forwardRef<
-  React.ElementRef<typeof SwitchPrimitives.Root>,
-  React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
->(({ className, ...props }, ref) => (
-  <SwitchPrimitives.Root
-    className={cn(
-      'peer inline-flex h-[30px] w-[50px] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors data-[state=checked]:bg-orange200 data-[state=unchecked]:bg-gray200',
-      className,
-    )}
-    {...props}
-    ref={ref}
-  >
-    <SwitchPrimitives.Thumb
-      className={cn(
-        'pointer-events-none block h-6 w-6 rounded-full bg-white shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-[22px] data-[state=unchecked]:translate-x-0',
-      )}
-    />
-  </SwitchPrimitives.Root>
-));
-Switch.displayName = SwitchPrimitives.Root.displayName;
+  return (
+    <div
+      className={`peer inline-flex h-[30px] w-[50px] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors ${
+        isOn ? 'bg-orange200' : 'bg-gray200'
+      }`}
+      onClick={toggleSwitch}
+    >
+      <motion.div
+        className="pointer-events-none block h-6 w-6 rounded-full bg-white shadow-lg ring-0"
+        layout
+        animate={{
+          x: isOn ? 22 : 0,
+        }}
+        transition={spring}
+      />
+    </div>
+  );
+}
 
-export { Switch };
+const spring = {
+  type: 'spring',
+  stiffness: 700,
+  damping: 30,
+};
