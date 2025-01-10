38193e342a49ea5ace79f0a7884295e0cea615fa
ayoooyh <syh9566@naver.com>
2025-01-10 13:10:27 +0900

    feat : 쿼리 적용 및 Link 수정

--- src/components/mypage/created-meetings/CreatedMeetings.tsx
+++ src/components/mypage/created-meetings/CreatedMeetings.tsx
@@ -1,61 +1,37 @@
+'use client';
+
 import GatheringCard from '@/components/mypage/created-meetings/gatheringCard/GatheringCard';
-import { IGathering } from '@/types/gathering.type';
-import { useEffect, useState } from 'react';
-import { getGatherings } from '@/apis/gatherings';
-import { getUserInfo } from '@/apis/userInfo';
 import Image from 'next/image';
 import emptyDudu from '../../../../public/images/mypage/dudu-empty.svg';
+import Link from 'next/link';
+import { useCreatedGatheringsQuery } from '@/hooks/mypage/queries/useGatheringsQuery';
 
-import { useRouter } from 'next/navigation';
 export default function CreatedMeetings() {
-  const router = useRouter();
-  const [gatherings, setGatherings] = useState<IGathering[]>([]);
-  const [userId, setUserId] = useState<number | null>(null);
+  const { data, isLoading } = useCreatedGatheringsQuery();
 
-  useEffect(() => {
-    const fetchUserInfo = async () => {
-      const data = await getUserInfo();
-      setUserId(data.id);
-    };
-    fetchUserInfo();
-  }, []);
+  if (isLoading) {
+    return <div>로딩 중...</div>;
+  }
 
-  useEffect(() => {
-    const fetchGatherings = async () => {
-      const data = await getGatherings();
-      const filteredData = data.filter((gathering) => gathering.createdBy === userId);
-      setGatherings(filteredData);
-    };
-    fetchGatherings();
-  }, [userId]);
-
-  const handleCreateGathering = () => {
-    router.push('/gatherings');
-  };
+  if (!data || data.length === 0) {
+    return (
+      <div className="flex flex-col justify-center items-center h-full gap-6">
+        <div className="flex flex-col justify-center items-center gap-4">
+          <Image src={emptyDudu} alt="empty" width={180} height={180} />
+          <p className="text-body-2-reading text-gray300">아직 만든 모임이 없어요</p>
+        </div>
+        <Link href="/gatherings" className="rounded-xl bg-gray950 px-5 py-2">
+          <span className="font-semibold text-label-normal text-gray50">모임 개설하기</span>
+        </Link>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col gap-4">
-      {gatherings.length === 0 ? (
-        <div className="flex flex-col justify-center items-center h-full gap-6">
-          <div className="flex flex-col justify-center items-center gap-4">
-            <Image src={emptyDudu} alt="empty" width={180} height={180} />
-            <div className="flex flex-col justify-center items-center ">
-              <p className="text-body-2-reading text-gray300">아직 만든 모임이 없어요</p>
-              <p className="text-body-2-reading text-gray300">새로운 모임을 만들어보세요</p>
-            </div>
-          </div>
-          <button className="rounded-xl bg-gray950 px-5 py-2">
-            <span
-              className="font-semibold text-label-normal text-gray50"
-              onClick={handleCreateGathering}
-            >
-              모임 개설하기
-            </span>
-          </button>
-        </div>
-      ) : (
-        gatherings.map((gathering) => <GatheringCard key={gathering.id} gathering={gathering} />)
-      )}
+      {data.map((gathering) => (
+        <GatheringCard key={gathering.id} gathering={gathering} />
+      ))}
     </div>
   );
 }
--- src/components/mypage/meetings/Meetings.tsx
+++ src/components/mypage/meetings/Meetings.tsx
@@ -1,42 +1,33 @@
+'use client';
+
 import GatheringCard from '@/components/mypage/created-meetings/gatheringCard/GatheringCard';
-import { IJoind } from '@/types/gathering.type';
-import { getJoined } from '@/apis/getJoined';
-import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import emptyDudu from '../../../../public/images/mypage/dudu-empty.svg';
+import { useJoinedGatheringsQuery } from '@/hooks/mypage/queries/useGatheringsQuery';
 
 export default function Meetings() {
-  const [gatheringJoined, setGatheringJoined] = useState<IJoind[]>([]);
+  const { data, isLoading } = useJoinedGatheringsQuery();
 
-  useEffect(() => {
-    const fetchGatheringJoined = async () => {
-      const token = localStorage.getItem('dudemeet-token');
-      console.log('저장된 토큰:', token);
+  if (isLoading) {
+    return <div>로딩 중...</div>;
+  }
 
-      try {
-        const data = await getJoined();
-        setGatheringJoined(data);
-      } catch (error) {
-        console.error('모임 조회 실패:', error);
-      }
-    };
-    fetchGatheringJoined();
-  }, []);
+  if (!data || data.length === 0) {
+    return (
+      <div className="flex flex-col justify-center items-center h-full gap-6">
+        <div className="flex flex-col justify-center items-center gap-4">
+          <Image src={emptyDudu} alt="empty" width={180} height={180} />
+          <p className="text-body-2-reading text-gray300">아직 내 모임이 없어요</p>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div>
-      {gatheringJoined.length === 0 ? (
-        <div className="flex flex-col justify-center items-center h-full gap-6">
-          <div className="flex flex-col justify-center items-center gap-4">
-            <Image src={emptyDudu} alt="empty" width={180} height={180} />
-            <p className="text-body-2-reading text-gray300">아직 내 모임이 없어요</p>
-          </div>
-        </div>
-      ) : (
-        gatheringJoined.map((gathering) => (
-          <GatheringCard key={gathering.id} gathering={gathering} />
-        ))
-      )}
+      {data.map((gathering) => (
+        <GatheringCard key={gathering.id} gathering={gathering} />
+      ))}
     </div>
   );
 }
