93f8d9115a4307bad310bb48e61ba76995bf2e6e
ayoooyh <syh9566@naver.com>
2025-01-10 17:50:42 +0900

    feat : 마이페이지 컴포넌트 분리

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,11 +1,9 @@
+import MypageContainer from '@/containers/mypage/MypageContainer';
 import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
 import { getQueryClient } from '@/hooks/mypage/queries/getQueryClient';
 import { getJoined } from '@/apis/getJoined';
 import { getGatherings } from '@/apis/gatherings';
 import { getUserInfo } from '@/apis/userInfo';
-import Header from '@/components/mypage/header/Header';
-import MyProfile from '@/components/mypage/myprofile/MyProfile';
-import RenderTab from '@/components/mypage/renderTab/RenderTab';
 
 export default async function Mypage() {
   const queryClient = getQueryClient();
@@ -30,11 +28,7 @@ export default async function Mypage() {
 
   return (
     <HydrationBoundary state={dehydratedState}>
-      <div className="h-screen max-w-[1200px] flex flex-col gap-4">
-        <Header />
-        <MyProfile />
-        <RenderTab />
-      </div>
+      <MypageContainer />
     </HydrationBoundary>
   );
 }
