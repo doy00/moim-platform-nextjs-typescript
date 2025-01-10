2a6cec63bd7c8dc9e1e0e5b945392c9b2b440604
ayoooyh <syh9566@naver.com>
2025-01-10 14:19:16 +0900

    feat : 만든 모임 토큰 추가

--- src/apis/gatherings.ts
+++ src/apis/gatherings.ts
@@ -2,6 +2,14 @@ import axiosInstance from './axiosInstance';
 import { IGathering } from '@/types/gathering.type';
 
 export const getGatherings = async (): Promise<IGathering[]> => {
-  const { data } = await axiosInstance.get('/gatherings');
+  const token = localStorage.getItem('dudemeet-token');
+  if (!token) {
+    throw new Error('인증 토큰이 없습니다.');
+  }
+  const { data } = await axiosInstance.get('/gatherings', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
   return data;
 };
