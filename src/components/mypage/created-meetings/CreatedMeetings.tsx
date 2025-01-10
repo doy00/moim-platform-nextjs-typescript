b59825374bd622f4976389907540e3e71988facf
ayoooyh <syh9566@naver.com>
2025-01-10 09:25:48 +0900

    feat : 모임 없을 시 화면 수정

--- src/components/mypage/created-meetings/CreatedMeetings.tsx
+++ src/components/mypage/created-meetings/CreatedMeetings.tsx
@@ -1,12 +1,14 @@
-'use client';
-
 import GatheringCard from '@/components/mypage/created-meetings/gatheringCard/GatheringCard';
 import { IGathering } from '@/types/gathering.type';
 import { useEffect, useState } from 'react';
 import { getGatherings } from '@/apis/gatherings';
-import { getUserInfo } from '@/apis/getUserInfo';
+import { getUserInfo } from '@/apis/userInfo';
+import Image from 'next/image';
+import emptyDudu from '../../../../public/images/mypage/dudu-empty.svg';
 
+import { useRouter } from 'next/navigation';
 export default function CreatedMeetings() {
+  const router = useRouter();
   const [gatherings, setGatherings] = useState<IGathering[]>([]);
   const [userId, setUserId] = useState<number | null>(null);
 
@@ -27,11 +29,33 @@ export default function CreatedMeetings() {
     fetchGatherings();
   }, [userId]);
 
+  const handleCreateGathering = () => {
+    router.push('/gatherings');
+  };
+
   return (
-    <div>
-      {gatherings.map((gathering) => (
-        <GatheringCard key={gathering.id} gathering={gathering} />
-      ))}
+    <div className="flex flex-col gap-4">
+      {gatherings.length === 0 ? (
+        <div className="flex flex-col justify-center items-center h-full gap-6">
+          <div className="flex flex-col justify-center items-center gap-4">
+            <Image src={emptyDudu} alt="empty" width={180} height={180} />
+            <div className="flex flex-col justify-center items-center ">
+              <p className="text-body-2-reading text-gray300">아직 만든 모임이 없어요</p>
+              <p className="text-body-2-reading text-gray300">새로운 모임을 만들어보세요</p>
+            </div>
+          </div>
+          <button className="rounded-xl bg-gray950 px-5 py-2">
+            <span
+              className="font-semibold text-label-normal text-gray50"
+              onClick={handleCreateGathering}
+            >
+              모임 개설하기
+            </span>
+          </button>
+        </div>
+      ) : (
+        gatherings.map((gathering) => <GatheringCard key={gathering.id} gathering={gathering} />)
+      )}
     </div>
   );
 }
