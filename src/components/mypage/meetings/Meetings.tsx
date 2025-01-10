323b3b6dfeea55e613a67ae0a4079da7112009e7
ayoooyh <syh9566@naver.com>
2025-01-10 09:22:55 +0900

    feat : 모임 없을 시 화면 수정

--- src/components/mypage/meetings/Meetings.tsx
+++ src/components/mypage/meetings/Meetings.tsx
@@ -2,6 +2,8 @@ import GatheringCard from '@/components/mypage/created-meetings/gatheringCard/Ga
 import { IJoind } from '@/types/gathering.type';
 import { getJoined } from '@/apis/getJoined';
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import emptyDudu from '../../../../public/images/mypage/dudu-empty.svg';
 
 export default function Meetings() {
   const [gatheringJoined, setGatheringJoined] = useState<IJoind[]>([]);
@@ -23,9 +25,18 @@ export default function Meetings() {
 
   return (
     <div>
-      {gatheringJoined.map((gathering) => (
-        <GatheringCard key={gathering.id} gathering={gathering} />
-      ))}
+      {gatheringJoined.length === 0 ? (
+        <div className="flex flex-col justify-center items-center h-full gap-6">
+          <div className="flex flex-col justify-center items-center gap-4">
+            <Image src={emptyDudu} alt="empty" width={180} height={180} />
+            <p className="text-body-2-reading text-gray300">아직 내 모임이 없어요</p>
+          </div>
+        </div>
+      ) : (
+        gatheringJoined.map((gathering) => (
+          <GatheringCard key={gathering.id} gathering={gathering} />
+        ))
+      )}
     </div>
   );
 }
