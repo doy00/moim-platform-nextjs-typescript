fc4f623b09b22ee01a26e94ec307476ffd4469ac
ayoooyh <syh9566@naver.com>
2025-01-10 17:49:27 +0900

    feat : qeuryProvider 수정

--- src/providers/mypage/QueryProviders.tsx
+++ src/providers/mypage/QueryProviders.tsx
@@ -4,7 +4,18 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactNode, useState } from 'react';
 
 export default function QueryProvider({ children }: { children: ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            retry: false,
+            staleTime: 1000 * 10,
+          },
+        },
+      }),
+  );
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
