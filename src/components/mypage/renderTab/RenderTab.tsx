a9a135dbe14d8fd533feaea193fdc7aada0e600b
ayoooyh <syh9566@naver.com>
2025-01-10 09:36:44 +0900

    feat : renderTab 추가

---
+++ src/components/mypage/renderTab/RenderTab.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { useState } from 'react';
+import Meetings from '@/components/mypage/meetings/Meetings';
+import CreatedMeetings from '@/components/mypage/created-meetings/CreatedMeetings';
+import Filter from '@/components/mypage/filter/Filter';
+import { motion, AnimatePresence } from 'framer-motion';
+
+export default function RenderTab() {
+  const [activeTab, setActiveTab] = useState('meetings');
+
+  const renderTab = () => {
+    return (
+      <div className="relative">
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={activeTab}
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -10 }}
+            transition={{ duration: 0.2 }}
+            className="absolute w-full"
+          >
+            {activeTab === 'meetings' ? (
+              <Meetings />
+            ) : activeTab === 'reviews' ? (
+              <div>나의 리뷰</div>
+            ) : (
+              <CreatedMeetings />
+            )}
+          </motion.div>
+        </AnimatePresence>
+      </div>
+    );
+  };
+
+  return (
+    <div className="flex flex-col gap-4 px-3">
+      <div className="flex px-4 justify-evenly items-center">
+        <button
+          className={`w-1/3 py-3.5 px-4 font-semibold text-sm ${
+            activeTab === 'meetings' ? 'border-b-2 border-[#4A4642] ' : 'text-[#C1BDB8] '
+          }`}
+          onClick={() => setActiveTab('meetings')}
+        >
+          내 모임
+        </button>
+        <button
+          className={`w-1/3 py-3.5 px-4 font-semibold text-sm ${
+            activeTab === 'reviews' ? 'border-b-2 border-[#4A4642] ' : 'text-[#C1BDB8]'
+          }`}
+          onClick={() => setActiveTab('reviews')}
+        >
+          내 리뷰
+        </button>
+        <button
+          className={`w-1/3 py-3.5 px-4 font-semibold text-sm ${
+            activeTab === 'created-meetings' ? 'border-b-2 border-[#4A4642] ' : 'text-[#C1BDB8]'
+          }`}
+          onClick={() => setActiveTab('created-meetings')}
+        >
+          만든 모임
+        </button>
+      </div>
+      <Filter />
+      {renderTab()}
+    </div>
+  );
+}
