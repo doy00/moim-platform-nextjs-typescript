751ea5344205d09cc1e43ee52d83284a6da87802
ayoooyh <syh9566@naver.com>
2025-01-10 09:20:09 +0900

    env : chadcn 설치

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 export default {
+  darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -12,18 +13,78 @@ export default {
         pretendard: 'var(--font-pretendard)',
       },
       fontSize: {
-        'title-1': ['24px', { lineHeight: '32px' }],
-        'title-2': ['22px', { lineHeight: '28px' }],
-        'heading-1': ['20px', { lineHeight: '26px' }],
-        'heading-2': ['18x', { lineHeight: '24px' }],
-        'body-1-normal': ['16px', { lineHeight: '20px' }],
-        'body-1-reading': ['16px', { lineHeight: '26px' }],
-        'body-2-normal': ['14px', { lineHeight: '20px' }],
-        'body-2-reading': ['14px', { lineHeight: '24px' }],
-        'label-normal': ['13px', { lineHeight: '18px' }],
-        'label-reading': ['13px', { lineHeight: '20px' }],
-        'caption-normal': ['12px', { lineHeight: '18px' }],
-        'caption-reading': ['12px', { lineHeight: '20px' }],
+        'title-1': [
+          '24px',
+          {
+            lineHeight: '32px',
+          },
+        ],
+        'title-2': [
+          '22px',
+          {
+            lineHeight: '28px',
+          },
+        ],
+        'heading-1': [
+          '20px',
+          {
+            lineHeight: '26px',
+          },
+        ],
+        'heading-2': [
+          '18x',
+          {
+            lineHeight: '24px',
+          },
+        ],
+        'body-1-normal': [
+          '16px',
+          {
+            lineHeight: '20px',
+          },
+        ],
+        'body-1-reading': [
+          '16px',
+          {
+            lineHeight: '26px',
+          },
+        ],
+        'body-2-normal': [
+          '14px',
+          {
+            lineHeight: '20px',
+          },
+        ],
+        'body-2-reading': [
+          '14px',
+          {
+            lineHeight: '24px',
+          },
+        ],
+        'label-normal': [
+          '13px',
+          {
+            lineHeight: '18px',
+          },
+        ],
+        'label-reading': [
+          '13px',
+          {
+            lineHeight: '20px',
+          },
+        ],
+        'caption-normal': [
+          '12px',
+          {
+            lineHeight: '18px',
+          },
+        ],
+        'caption-reading': [
+          '12px',
+          {
+            lineHeight: '20px',
+          },
+        ],
       },
       screens: {
         xs: '460px',
@@ -79,13 +140,57 @@ export default {
         textAlter1: '#3A3A40',
         textAlter2: '#42424A',
         textAlter3: '#4D4E57',
-
         background100: '#FFFFFF',
         background200: '#FBFAF9',
         background300: '#FAF6F3',
         background400: '#F2EFEB',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          '1': 'hsl(var(--chart-1))',
+          '2': 'hsl(var(--chart-2))',
+          '3': 'hsl(var(--chart-3))',
+          '4': 'hsl(var(--chart-4))',
+          '5': 'hsl(var(--chart-5))',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
