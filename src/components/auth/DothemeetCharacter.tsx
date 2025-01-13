8096e0e5f4463f23567a2b392f1923c0a2a67c7d
Eun Oh <bdohhhhh@gmail.com>
2025-01-13 17:07:13 +0900

    chore: main logo 변경

--- src/components/auth/DothemeetCharacter.tsx
+++ src/components/auth/DothemeetCharacter.tsx
@@ -4,8 +4,8 @@ export default function DothemeetCharacter() {
   return (
     <div className="relative w-full flex flex-col items-center justify-center">
       <div className="absolute w-[155px] h-[155px] bg-background400 rounded-full -translate-y-7"></div>
-      <div className="w-[207px] h-[207px] relative">
-        <Image src={'/svgs/svg_dothemeet.svg'} alt="dothemeet" fill />
+      <div className="w-[181px] h-[203px] relative">
+        <Image src={'/svgs/svg_dothemeet_full.svg'} alt="dothemeet" fill />
       </div>
     </div>
   );
