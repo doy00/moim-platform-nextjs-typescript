761554ccff032d7f6d03e15c4e46a1ec45b9b767
ayoooyh <syh9566@naver.com>
2025-01-15 13:30:35 +0900

    move : 불필요한 폴더 삭제로 파일 이동

--- src/components/mypage/CreatedMeetings.tsx
+++ src/components/mypage/CreatedMeetings.tsx
@@ -2,7 +2,7 @@
 
 import GatheringCard from '@/components/mypage/gatheringCard/GatheringCard';
 import Image from 'next/image';
-import emptyDudu from '../../../../public/images/mypage/dudu-empty.svg';
+import emptyDudu from '../../../public/images/mypage/dudu-empty.svg';
 import Link from 'next/link';
 import { useCreatedGatheringsQuery } from '@/hooks/mypage/queries/useGatheringsQuery';
 import { motion } from 'framer-motion';
