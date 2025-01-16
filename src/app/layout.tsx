65cd6a5ad096269fb4aa3c4b1aa6f2194b7dd44e
doy00 <dhkm201@gmail.com>
2025-01-16 10:07:36 +0900

    etc: {children} 중복으로 들어갔던거 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={`layout font-pretendard antialiased ${pretendard.variable}`}>{children}
+      <body className={`layout font-pretendard antialiased ${pretendard.variable}`}>
         <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
