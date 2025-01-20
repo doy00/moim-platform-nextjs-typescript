43c3bda5009d4f3be656e1cb47405f424195c8db
ayoooyh <syh9566@naver.com>
2025-01-20 17:12:49 +0900

    chore : webpack 설치

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,12 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   images: {
     domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'],
+  },
   /* config options here */
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"],
+      use: ['@svgr/webpack'],
     });
     return config;
   },
