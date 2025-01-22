e2921e651a35485810283d5c4e497a92f286774c
jakesungjaekim@gmail.com <jakesungjaekim@gmail.com>
2025-01-22 13:04:44 +0900

    etc: home Gnb, url 구조 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
-'use client';
-
-import HomeGnb from '@/components/home/HomeGnb';
+import '../styles/globals.css';
 import QueryProvider from '@/libs/detail/QueryProvider';
 import localFont from 'next/font/local';
-import { usePathname } from 'next/navigation';
-import '../styles/globals.css';
+import HomeGnb from '@/components/home/HomeGnb';
 
 const pretendard = localFont({
   src: '../../public/fonts/PretendardVariable.woff2',
@@ -19,15 +16,9 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // HomeGnb를 보여줄 경로 목록
-  const pathname = usePathname();
-  const showGnbPaths = ['/home', '/favorites', '/mypage'];
-  const showGnb = showGnbPaths.includes(pathname);
-
   return (
     <html lang="ko">
       <head>
-        {/* Daum 우편번호 API 스크립트 로드 */}
         <script
           src="https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"
           async
@@ -36,7 +27,7 @@ export default function RootLayout({
       <body className={`bg-gray-200 font-pretendard antialiased ${pretendard.variable}`}>
         <QueryProvider>
           <div className="layout">{children}</div>
-          {showGnb && <HomeGnb />}
+          <HomeGnb />
         </QueryProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import React from 'react'
+
+//Container
+import HomeContainer from '@/containers/home/HomeContainer'
+
 export default function Home() {
-  return <></>;
+  return (
+    <div className="px-2 py-3 max-w-[500px] h-auto bg-white">
+      <HomeContainer />
+    </div>
+  );
 }
