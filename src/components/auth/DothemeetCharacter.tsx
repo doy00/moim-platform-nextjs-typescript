4f08f3c78645023538baabd6f00ae45086238e5e
Eun Oh <bdohhhhh@gmail.com>
2025-01-23 11:53:48 +0900

    chore: 두더밋로고 분기처리

--- src/components/auth/DothemeetCharacter.tsx
+++ src/components/auth/DothemeetCharacter.tsx
@@ -1,10 +1,28 @@
+import { cn } from '@/lib/utils';
 import Image from 'next/image';
 
-export default function DothemeetCharacter() {
+interface DothemeetCharacterProps {
+  isFull: boolean;
+}
+
+export default function DothemeetCharacter({ isFull }: DothemeetCharacterProps) {
   return (
     <div className="relative w-full flex flex-col items-center justify-center">
       <div className="w-[181px] h-[203px] relative">
-        <Image src={'/svgs/svg_dothemeet_full.svg'} alt="dothemeet" fill priority />
+        <Image
+          src={'/svgs/svg_dothemeet_full.svg'}
+          alt="dothemeet"
+          fill
+          priority
+          className={cn(isFull ? 'opacity-100' : 'opacity-0')}
+        />
+        <Image
+          src={'/svgs/svg_dothemeet_circle.svg'}
+          alt="dothemeet"
+          fill
+          priority
+          className={cn(isFull ? 'opacity-0' : 'opacity-100')}
+        />
       </div>
     </div>
   );
