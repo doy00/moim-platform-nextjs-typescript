51ed634b44f80c4e1640ce715caa259fab8f53e4
Eun Oh <bdohhhhh@gmail.com>
2025-01-27 22:22:56 +0900

    chore: post register(email) route handler 작성

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,58 @@
+import { TAuthSignUpInputs, TMe } from '@/types/auth/auth.type';
+import { createClient } from '@/utils/supabase/server';
+import { PostgrestError } from '@supabase/supabase-js';
+import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
+  const { email, password, nickname, position, introduction, tags }: TAuthSignUpInputs =
+    await request.json();
+
+  const cookieStore = await cookies();
+  const supabase = createClient(cookieStore);
+
+  if (!email || !password || !nickname) {
+    return NextResponse.json(
+      { message: '이메일, 비밀번호, 닉네임은 필수 입력 항목입니다.' },
+      { status: 400 },
+    );
+  }
+
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        nickname,
+        position,
+        introduction,
+        tags,
+      },
+    },
+  });
+
+  if (error) {
+    return NextResponse.json({ message: error?.message }, { status: 401 });
+  }
+
+  if (!user) {
+    return NextResponse.json({ message: '회원가입에 실패했습니다' }, { status: 404 });
+  }
+
+  const { data: me, error: userError }: { data: TMe | null; error: PostgrestError | null } =
+    await supabase.from('users').select('*').eq('id', user.id).single();
+
+  if (userError) {
+    console.error(userError);
+    return NextResponse.json({ message: userError?.message }, { status: 401 });
+  }
+
+  if (!me) {
+    return NextResponse.json({ message: '회원가입에 실패했습니다' }, { status: 404 });
+  }
+
+  return NextResponse.json(me, { status: 200 });
+}
