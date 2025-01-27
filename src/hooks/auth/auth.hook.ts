da95bff2814a9fbd49aa950a46d1999964678794
Eun Oh <bdohhhhh@gmail.com>
2025-01-27 22:45:03 +0900

    chore: auth hook 수정

--- src/hooks/auth/auth.hook.ts
+++ src/hooks/auth/auth.hook.ts
@@ -1,25 +1,19 @@
 'use client';
 
-import { getMe, postSetCookie, postSignIn, postSignUp, putMe } from '@/apis/auth/auth.api';
+import { deleteSignOut, getMe, postSignIn, postSignUp, putMe } from '@/apis/auth/auth.api';
 import { QUERY_KEY_ME } from '@/constants/auth/auth.const';
 import type {
   TAuthSignInInputs,
   TAuthSignUpInputs,
   TMe,
-  TMeResponse,
   TPutMeInputs,
-  TSetCookieInputs,
-  TSetCookieResponse,
-  TSignInResponse,
+  TSignOutResponse,
   TSignUpResponse,
 } from '@/types/auth/auth.type';
 import type { TError } from '@/types/auth/error.type';
-import { removeLocalStorageItem, setLocalStorageItem } from '@/utils/auth/auth-client.util';
-import { deleteCookie } from '@/utils/auth/auth-server.util';
 import type { UseMutationResult, UseQueryResult } from '@tanstack/react-query';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { useRouter } from 'next/navigation';
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 type Debounce<T extends unknown[]> = (...args: T) => void;
 
@@ -48,29 +42,29 @@ export function useSignUpMutation(): UseMutationResult<TSignUpResponse, TError, 
   });
 }
 
-export function useSignInMutation(): UseMutationResult<TSignInResponse, TError, TAuthSignInInputs> {
-  return useMutation<TSignInResponse, TError, TAuthSignInInputs>({
+export function useSignInMutation(): UseMutationResult<TMe, TError, TAuthSignInInputs> {
+  const queryClient = useQueryClient();
+  return useMutation<TMe, TError, TAuthSignInInputs>({
     mutationFn: postSignIn,
-    onSuccess: (data) => {
-      setLocalStorageItem('accessToken', data.data.accessToken);
-      setLocalStorageItem('refreshToken', data.data.refreshToken);
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY_ME] });
     },
   });
 }
 
-export function useSetCookieMutation(): UseMutationResult<
-  TSetCookieResponse,
-  TError,
-  TSetCookieInputs
-> {
-  return useMutation<TSetCookieResponse, TError, TSetCookieInputs>({
-    mutationFn: postSetCookie,
+export function useSignOutMutation(): UseMutationResult<TSignOutResponse, TError, void> {
+  const queryClient = useQueryClient();
+  return useMutation<TSignOutResponse, TError, void>({
+    mutationFn: deleteSignOut,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY_ME] });
+    },
   });
 }
 
-export function usePutMeMutation(): UseMutationResult<TMeResponse, TError, TPutMeInputs> {
+export function usePutMeMutation(): UseMutationResult<TMe, TError, TPutMeInputs> {
   const queryClient = useQueryClient();
-  return useMutation<TMeResponse, TError, TPutMeInputs>({
+  return useMutation<TMe, TError, TPutMeInputs>({
     mutationFn: putMe,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [QUERY_KEY_ME] });
@@ -93,42 +87,28 @@ interface UseAuthProps {
 // me 객체 접근과, 로그아웃을 편리하게 사용하기 위한 훅
 export function useAuth(props?: UseAuthProps) {
   const { enabled = true } = props || {};
-  const queryClient = useQueryClient();
+  const [isMutationPending, setIsMutationPending] = useState(false);
   const { data: me, isLoading: isMeLoading, error } = useMeQuery(enabled);
-  const router = useRouter();
-
-  const signOut = useCallback(() => {
-    removeLocalStorageItem('accessToken');
-    removeLocalStorageItem('refreshToken');
-    deleteCookie('accessToken');
-    deleteCookie('refreshToken');
-    queryClient.invalidateQueries({ queryKey: [QUERY_KEY_ME] });
-    queryClient.setQueryData([QUERY_KEY_ME], null);
-    router.refresh();
-  }, [queryClient, router]);
+  const {
+    mutate: signOut,
+    isPending: isSignOutPending,
+    error: signOutError,
+  } = useSignOutMutation();
+  const {
+    mutate: updateMe,
+    isPending: isUpdateMePending,
+    error: updateMeError,
+  } = usePutMeMutation();
 
   useEffect(() => {
-    if (!error) return;
-    if (error.message !== 'Unauthorized') {
-      console.log(error);
-    }
-  }, [error, router]);
+    if (!error && !signOutError && !updateMeError) return;
+    console.log(error?.message);
+  }, [error, signOutError, updateMeError]);
 
-  return { me, signOut, isMeLoading };
+  useEffect(() => {
+    if (!isSignOutPending && !isUpdateMePending) return;
+    setIsMutationPending(true);
+  }, [isSignOutPending, isUpdateMePending]);
+
+  return { me, signOut, updateMe, isMeLoading, isMutationPending };
 }
-
-// // signout 엔드포인트가 추후 없어진다면 이 뮤테이션도 없어질 예정
-// export function useSignOutMutation(): UseMutationResult<TSignOutResponse, TError, void> {
-//   const queryClient = useQueryClient();
-//   return useMutation<TSignOutResponse, TError, void>({
-//     mutationFn: postSignOut,
-//     onSuccess: () => {
-//       removeLocalStorageItem('accessToken');
-//       removeLocalStorageItem('refreshToken');
-//       deleteCookie('accessToken');
-//       deleteCookie('refreshToken');
-//       queryClient.invalidateQueries({ queryKey: [QUERY_KEY_ME] });
-//       queryClient.setQueryData([QUERY_KEY_ME], null);
-//     },
-//   });
-// }
