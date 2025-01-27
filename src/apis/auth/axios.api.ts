2dfc3a1e2798fc63979103a10fbbbb7837406015
Eun Oh <bdohhhhh@gmail.com>
2025-01-27 22:44:32 +0900

    chore: auth, axios api 수정

--- src/apis/auth/axios.api.ts
+++ src/apis/auth/axios.api.ts
@@ -1,4 +1,3 @@
-import { getLocalStorageItem } from '@/utils/auth/auth-client.util';
 import type { AxiosError, AxiosResponse } from 'axios';
 import axios from 'axios';
 
@@ -6,21 +5,20 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL,
 });
 
-api.interceptors.request.use(async (config) => {
-  if (typeof window !== 'undefined') {
-    const token = getLocalStorageItem('access_token');
-    if (token) {
-      config.headers.set('Authorization', `Bearer ${token}`);
-    }
-  }
-  // else {
-  //   const token = await getCookie('accessToken');
-  //   if (token) {
-  //     config.headers.set('Authorization', `Bearer ${token}`);
-  //   }
-  // }
-  return config;
-});
+// api.interceptors.request.use(async (config) => {
+//   if (typeof window !== 'undefined') {
+//     const token = getLocalStorageItem('accessToken');
+//     if (token) {
+//       config.headers.set('Authorization', `Bearer ${token}`);
+//     }
+//   } else {
+//     const token = await getCookie('accessToken');
+//     if (token) {
+//       config.headers.set('Authorization', `Bearer ${token}`);
+//     }
+//   }
+//   return config;
+// });
 
 api.interceptors.response.use(
   <T>(response: AxiosResponse<T>) => response.data as T,
