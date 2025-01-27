195da77a5893cf3e743e09fbd68ca1fc98e7f8e1
Eun Oh <bdohhhhh@gmail.com>
2025-01-27 22:22:56 +0900

    chore: post register(email) route handler 작성

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,5 +1,4 @@
 import { TAuthSignUpInputs, TMe } from '@/types/auth/auth.type';
-import { setCookie } from '@/utils/auth/auth-server.util';
 import { createClient } from '@/utils/supabase/server';
 import { PostgrestError } from '@supabase/supabase-js';
 import { cookies } from 'next/headers';
@@ -14,28 +13,13 @@ export async function POST(request: NextRequest) {
 
   if (!email || !password || !nickname) {
     return NextResponse.json(
-      { message: '이메일, 비밀번호, 닉네임은 필수 입력 항목입니다' },
+      { message: '이메일, 비밀번호, 닉네임은 필수 입력 항목입니다.' },
       { status: 400 },
     );
   }
 
-  const { data: existingUsers, error: existingUsersError } = await supabase
-    .from('users')
-    .select('*');
-
-  if (existingUsersError) {
-    console.log('existingUsersError ====>', existingUsersError);
-    return NextResponse.json({ message: '서버에 문제가 발생했습니다' }, { status: 500 });
-  }
-
-  const existingUserNickname = existingUsers.find((user) => user.nickname === nickname);
-
-  if (existingUserNickname) {
-    return NextResponse.json({ message: '중복되는 닉네임이 있어요' }, { status: 400 });
-  }
-
   const {
-    data: { user, session },
+    data: { user },
     error,
   } = await supabase.auth.signUp({
     email,
@@ -43,15 +27,14 @@ export async function POST(request: NextRequest) {
     options: {
       data: {
         nickname,
+        position,
+        introduction,
+        tags,
       },
     },
   });
 
   if (error) {
-    console.log('error ====>', error);
-    if (error.message === 'User already registered') {
-      return NextResponse.json({ message: '이미 가입된 계정이에요' }, { status: 400 });
-    }
     return NextResponse.json({ message: error?.message }, { status: 401 });
   }
 
@@ -59,58 +42,17 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({ message: '회원가입에 실패했습니다' }, { status: 404 });
   }
 
-  const { data: existingUser, error: existingUserError } = await supabase
-    .from('users')
-    .select('*')
-    .eq('email', email)
-    .single();
-
-  if (existingUser) {
-    return NextResponse.json({ message: '이미 가입된 이메일입니다' }, { status: 400 });
-  }
-
-  if (
-    existingUserError &&
-    existingUserError.message !== 'JSON object requested, multiple (or no) rows returned'
-  ) {
-    return NextResponse.json({ message: '서버에 문제가 발생했습니다' }, { status: 500 });
-  }
-
-  const { data: userData, error: userError }: { data: TMe | null; error: PostgrestError | null } =
-    await supabase
-      .from('users')
-      .upsert({
-        email,
-        nickname,
-        position,
-        introduction,
-        tags,
-      })
-      .select()
-      .single();
+  const { data: me, error: userError }: { data: TMe | null; error: PostgrestError | null } =
+    await supabase.from('users').select('*').eq('id', user.id).single();
 
   if (userError) {
-    // console.log('userError ====>', userError);
+    console.error(userError);
+    return NextResponse.json({ message: userError?.message }, { status: 401 });
+  }
+
+  if (!me) {
     return NextResponse.json({ message: '회원가입에 실패했습니다' }, { status: 404 });
   }
 
-  if (!userData) {
-    return NextResponse.json({ message: '회원가입에 실패했습니다' }, { status: 404 });
-  }
-
-  if (session?.access_token) {
-    setCookie({
-      name: 'access_token',
-      value: session.access_token,
-      maxAge: 60 * 60,
-    });
-  }
-
-  return NextResponse.json(
-    {
-      me: userData,
-      tokens: { accessToken: session?.access_token, refreshToken: session?.refresh_token },
-    },
-    { status: 200 },
-  );
+  return NextResponse.json(me, { status: 200 });
 }
