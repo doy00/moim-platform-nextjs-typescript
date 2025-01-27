8014e84e421cc75144f033aa58d1eb96bf497bff
Eun Oh <bdohhhhh@gmail.com>
2025-01-27 22:24:33 +0900

    chore: auth constants 수정

--- src/constants/auth/auth.const.ts
+++ src/constants/auth/auth.const.ts
@@ -1,3 +1,2 @@
 export const QUERY_KEY_ME = 'me';
-export const PUBLIC_URL = process.env.NEXT_PUBLIC_API_URL;
-export const PROVIDERS = { kakao: 'kakao' } as const;
+export const PUBLIC_URL = process.env.NEXT_PUBLIC_BASE_URL;
