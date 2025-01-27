aeeaa177aa549e01db432e2f8858ce86d7bf69aa
ayoooyh <syh9566@naver.com>
2025-01-27 17:15:30 +0900

    feat : 임시로그인 주석처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 //Container
 import HomeContainer from '@/containers/home/HomeContainer';
+// import TemporaryLogin from '@/containers/mypage/TemporaryLogin';
 
 export default function Home() {
   return (
-    <div className="px-2 py-3 max-w-[500px] h-auto bg-white">
+    <div className="px-2 py-3 mx-auto sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg h-auto bg-white">
       <HomeContainer />
+      {/* <TemporaryLogin /> */}
     </div>
   );
 }
