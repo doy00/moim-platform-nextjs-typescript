6fe452a2de7198de411a1260e17c36d443f2d0f9
Eun Oh <bdohhhhh@gmail.com>
2025-01-27 22:23:27 +0900

    chore: delete signout route handler 작성

--- src/app/api/auth/signout/route.ts
+++ src/app/api/auth/signout/route.ts
@@ -1,15 +1,12 @@
-import { deleteCookie } from '@/utils/auth/auth-server.util';
 import { createClient } from '@/utils/supabase/server';
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
-export async function DELETE() {
+export async function POST() {
   const cookieStore = await cookies();
   const supabase = createClient(cookieStore);
 
   const { error } = await supabase.auth.signOut();
-
-  deleteCookie('access_token');
 
   if (error) {
     return NextResponse.json({ message: error.message }, { status: 500 });
