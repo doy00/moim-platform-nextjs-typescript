0a25978da680fb3c6e00d77fc1ae45977a1f7be9
EunOh <bdohhhhh@gmail.com>
2025-01-28 11:49:46 +0900

    feat: moims route handler 작성(get, post)

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,18 +1,57 @@
 'use client';
 
-import { useEffect } from 'react';
+import api from '@/apis/auth/axios.api';
+import { useState } from 'react';
+
+export const postMoims = (data: any) => {
+  const url = '/api/moims';
+  return api.post<any, any>(url, data, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+};
 
 function page() {
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('/api/moims?page=1');
-      const data = await response.json();
-      console.log(data);
-    };
-    fetchData();
-  }, []);
+  const [file, setFile] = useState<File | null>(null);
 
-  return <div>page</div>;
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setFile(file);
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!file) return;
+    const formData = new FormData();
+
+    const moimData = {
+      title: '다시테스트',
+      content: '다시테스트내용',
+      roadAddress: '다시테스트주소',
+      recruitmentDeadline: new Date('2025-02-15').toISOString(),
+      startDate: new Date('2025-02-15').toISOString(),
+      endDate: new Date('2025-03-15').toISOString(),
+      minParticipants: 6,
+      maxParticipants: 16,
+      moimType: 'PROJECT',
+      status: 'RECRUIT',
+    };
+
+    formData.append('moim_image', file);
+    formData.append('moim_json', JSON.stringify(moimData));
+    const response = await postMoims(formData);
+    console.log(response);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="file" onChange={handleFileChange} />
+      <button type="submit">제출</button>
+    </form>
+  );
 }
 
 export default page;
--- src/types/supabase/supabase-custom.type.ts
+++ src/types/supabase/supabase-custom.type.ts
@@ -5,3 +5,24 @@ export type TUsers = Tables<'users'>;
 export type TReviews = Tables<'reviews'>;
 export type TLikedMoims = Tables<'liked_moims'>;
 export type TParticipatedMoims = Tables<'participated_moims'>;
+
+export type TMoimClient = {
+  title: string;
+  content: string;
+  address: string;
+  recruitmentDeadline: string;
+  startDate: string;
+  endDate: string;
+  minParticipants: number;
+  maxParticipants: number;
+  moimType: string;
+  status: string;
+  likes: number;
+  participants: number;
+  reviewsCount: number;
+};
+
+export type TMoimsJoined = TMoims & {
+  reviews: TReviews[];
+  participated_moims: TParticipatedMoims[];
+};
