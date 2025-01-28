a093444fa87b3e1b756259e17a4b4d61e0ef6236
EunOh <bdohhhhh@gmail.com>
2025-01-28 16:04:07 +0900

    chore: signout 메소드 delete로 수정

--- src/app/api/auth/signout/route.ts
+++ src/app/api/auth/signout/route.ts
@@ -2,7 +2,7 @@ import { createClient } from '@/utils/supabase/server';
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
-export async function POST() {
+export async function DELETE() {
   const cookieStore = await cookies();
   const supabase = createClient(cookieStore);
 
