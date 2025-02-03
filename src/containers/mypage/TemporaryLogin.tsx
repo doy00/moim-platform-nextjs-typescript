b84cce562535fc23cad1432a0f5f5e13615b5a1d
ayoooyh <syh9566@naver.com>
2025-02-03 15:17:38 +0900

    feat : 임시로그인 주석처리

--- src/containers/mypage/TemporaryLogin.tsx
+++ src/containers/mypage/TemporaryLogin.tsx
@@ -1,30 +1,30 @@
-'use client';
+// 'use client';
 
-import { login } from '@/apis/login';
-import { ILogin } from '@/types/mypage/login';
-import { useRouter } from 'next/navigation';
-import { setCookie, getCookie } from 'cookies-next';
+// import { login } from '@/apis/login';
+// import { ILogin } from '@/types/mypage/login';
+// import { useRouter } from 'next/navigation';
+// import { setCookie, getCookie } from 'cookies-next';
 
-const MOCK_LOGIN: ILogin = {
-  email: 'dude@email.com',
-  password: 'test1234',
-};
+// const MOCK_LOGIN: ILogin = {
+//   email: 'dude@email.com',
+//   password: 'test1234',
+// };
 
-export default function TemporaryLogin() {
-  const router = useRouter();
+// export default function TemporaryLogin() {
+//   const router = useRouter();
 
-  const handleLogin = async () => {
-    const response = await login(MOCK_LOGIN);
+//   const handleLogin = async () => {
+//     const response = await login(MOCK_LOGIN);
 
-    const token = response.data.accessToken;
+//     const token = response.data.accessToken;
 
-    setCookie('accessToken', token, { path: '/' });
-    router.push('/mypage');
-  };
+//     setCookie('accessToken', token, { path: '/' });
+//     router.push('/mypage');
+//   };
 
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <button onClick={handleLogin}>로그인</button>
-    </div>
-  );
-}
+//   return (
+//     <div className="flex justify-center items-center h-screen">
+//       <button onClick={handleLogin}>로그인</button>
+//     </div>
+//   );
+// }
