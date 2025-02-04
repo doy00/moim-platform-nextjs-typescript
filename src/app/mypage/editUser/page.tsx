639f9d4eede0eb756cdaad240251d46bff125c4d
ayoooyh <syh9566@naver.com>
2025-02-04 17:23:39 +0900

    style : 불필요한 코드 정리

--- src/app/mypage/editUser/page.tsx
+++ src/app/mypage/editUser/page.tsx
@@ -398,28 +398,3 @@ export default function EditUser() {
     </>
   );
 }
-
-// 'use client';
-// import { LoadingAnimation } from '@/components/mypage/LoadingAnimation';
-// import Link from 'next/link';
-// import { motion } from 'framer-motion';
-
-// export default function Review() {
-//   return (
-//     <div>
-//       <div className="flex flex-col gap-5 justify-center items-center h-screen">
-//         <LoadingAnimation />
-//         <p className="text-heading-2 text-gray500">회원정보 수정 개발중입니다</p>
-//         <motion.div
-//           whileHover={{ scale: 1.2 }}
-//           whileTap={{ scale: 0.9 }}
-//           transition={{ type: 'spring', stiffness: 400, damping: 17 }}
-//         >
-//           <Link href="/mypage" className="rounded-xl bg-gray950 px-5 py-2">
-//             <span className="font-semibold text-label-normal text-gray50">뒤로가기</span>
-//           </Link>
-//         </motion.div>
-//       </div>
-//     </div>
-//   );
-// }
