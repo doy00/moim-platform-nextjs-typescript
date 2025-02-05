399393dff17e531168acacd6d85822a033261a27
Eun Oh <bdohhhhh@gmail.com>
2025-02-05 09:42:44 +0900

    fix: 리뷰작성, 참여 안되는 에러 수정

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -5,11 +5,26 @@ import { useAuth } from '@/hooks/auth/auth.hook';
 export default function TestPage() {
   const { me, isMeLoading, signOut } = useAuth();
 
+  const handleTest = async () => {
+    const response = await fetch(`/api/moims/cb2cf966-02e3-4d71-bfb2-e14c60dbb85a/review`, {
+      method: 'POST',
+      body: JSON.stringify({
+        review: '테스트 리뷰',
+        rate: 'SOSO',
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <div>
       <button onClick={signOut}>로그아웃</button>
       <div>
         <p>{me?.email}</p>
+      </div>
+      <div>
+        <button onClick={handleTest}>리뷰작성테스트</button>
       </div>
     </div>
   );
