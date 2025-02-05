59238a36c754383994343c4fe80d97691e561366
Eun Oh <bdohhhhh@gmail.com>
2025-02-05 10:45:23 +0900

    chore: 내가작성한리뷰 리턴데이터 수정

--- src/app/api/reviews/my/route.ts
+++ src/app/api/reviews/my/route.ts
@@ -1,4 +1,6 @@
-import { TReviewWithMoim } from '@/types/supabase/supabase-custom.type';
+import { TMe } from '@/types/auth/auth.type';
+import { TReviewWithMoim, TReviewWithMoimClient } from '@/types/supabase/supabase-custom.type';
+import { mapMoimsToClient } from '@/utils/common/mapMoims';
 import { createClient } from '@/utils/supabase/server';
 import { PostgrestError } from '@supabase/supabase-js';
 import { cookies } from 'next/headers';
@@ -23,17 +25,50 @@ export async function GET() {
   }
 
   const {
+    data: foundUser,
+    error: foundUserError,
+  }: { data: TMe | null; error: PostgrestError | null } = await supabase
+    .from('users')
+    .select('*')
+    .eq('email', user.email)
+    .single();
+
+  if (foundUserError) {
+    return NextResponse.json({ message: foundUserError?.message }, { status: 401 });
+  }
+
+  if (!foundUser) {
+    return NextResponse.json({ message: '사용자 정보가 없어요' }, { status: 401 });
+  }
+
+  const {
     data: reviewsWithMoim,
     error: reviewsError,
   }: { data: TReviewWithMoim[] | null; error: PostgrestError | null } = await supabase
     .from('reviews')
-    .select('*, moims (*)')
-    .eq('user_uuid', user.id)
+    .select(
+      '*, moims (*, reviews (user_uuid, review, rate, user_email, user_image, user_nickname), participated_moims (user_uuid, user_email, user_image, user_nickname), liked_moims (user_uuid))',
+    )
+    .eq('user_uuid', foundUser.id)
     .order('created_at', { ascending: false });
 
   if (reviewsError) {
     return NextResponse.json({ message: reviewsError?.message }, { status: 401 });
   }
 
-  return NextResponse.json(reviewsWithMoim, { status: 200 });
+  if (!reviewsWithMoim) {
+    return NextResponse.json({ message: '리뷰가 없어요' }, { status: 404 });
+  }
+
+  const mappedReviewsWithMoim: TReviewWithMoimClient[] = reviewsWithMoim.map((review) => ({
+    userUuid: review.user_uuid,
+    review: review.review,
+    rate: review.rate,
+    userEmail: review.user_email,
+    userImage: review.user_image,
+    userNickname: review.user_nickname,
+    moims: mapMoimsToClient([review.moims])[0],
+  }));
+
+  return NextResponse.json(mappedReviewsWithMoim, { status: 200 });
 }
