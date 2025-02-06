91253b460f2f413eff11732d2cb0760f2942eed5
Eun Oh <bdohhhhh@gmail.com>
2025-02-06 22:25:15 +0900

    chore: refresh_token 테스트중

--- src/app/api/moims/liked/route.ts
+++ src/app/api/moims/liked/route.ts
@@ -4,6 +4,7 @@ import {
   TMoimClient,
   TMoimsJoined,
 } from '@/types/supabase/supabase-custom.type';
+import { setCookie } from '@/utils/auth/auth-server.util';
 import { mapMoimsToClient } from '@/utils/common/mapMoims';
 import { createClient } from '@/utils/supabase/server';
 import { AuthError, PostgrestError, User } from '@supabase/supabase-js';
@@ -18,22 +19,66 @@ export async function GET(req: NextRequest) {
   const supabase = createClient(cookieStore);
   const authorization = (await headers()).get('authorization');
   const token = authorization?.split(' ')[1] ?? null;
+  const refreshToken = cookieStore.get('refresh_token')?.value ?? null;
+
+  console.log('refreshToken????? ====>', refreshToken);
 
   let user: User | null;
   let error: AuthError | null;
-  if (token) {
-    ({
-      data: { user },
-      error,
-    } = await supabase.auth.getUser(token));
-  } else {
+
+  if (!token) {
     ({
       data: { user },
       error,
     } = await supabase.auth.getUser());
+  } else {
+    ({
+      data: { user },
+      error,
+    } = await supabase.auth.getUser(token));
+  }
+
+  console.log('error????? ====>', error?.code);
+
+  // TODO: refreshToken 이 자꾸 invalid 라는데,
+  // 실제로 로그인 후 한시간 기다려서 access_token 이 만료된 뒤에,
+  // 진짜 refresh_token이 invalid 한것인지, 즉 내가 로직을 잘못 짠 것인지 뭔지 테스트 필요
+  // 그리고 getUser 로직이 route handler 마다 반복되므로, api/auth/me 로 한번 get을 날려서 처리하는 방법은 없는지 확인 필요
+  if (error?.code === 'bad_jwt' && refreshToken) {
+    console.log('why refreshToken????? ====>', refreshToken);
+    const { data: refreshData, error: refreshError } = await supabase.auth.refreshSession({
+      refresh_token: refreshToken,
+    });
+
+    console.log('refreshError????????? ====>', refreshError);
+
+    if (refreshData?.session?.access_token && refreshData?.session?.refresh_token) {
+      const { data: userData, error: userError } = await supabase.auth.getUser(
+        refreshData.session.access_token,
+      );
+      user = userData.user;
+
+      setCookie({
+        name: 'access_token',
+        value: refreshData?.session?.access_token,
+        maxAge: 60 * 60,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+      });
+      setCookie({
+        name: 'refresh_token',
+        value: refreshData?.session?.refresh_token,
+        maxAge: 60 * 60 * 24 * 30,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+      });
+    }
   }
 
   if (error) {
+    // console.log('error????? ====>', error);
     return NextResponse.json({ message: error?.message }, { status: 401 });
   }
 
