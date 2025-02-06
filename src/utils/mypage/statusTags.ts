7f42a88794b21ec4486a2fabe4e5e9d47bb38b7c
ayoooyh <syh9566@naver.com>
2025-02-06 14:40:48 +0900

    feat : status 타입 에러로 수정

--- src/utils/mypage/statusTags.ts
+++ src/utils/mypage/statusTags.ts
@@ -4,13 +4,15 @@ import projectIcon from '@images/mypage/puzzle-on.svg';
 import studyIcon from '@images/mypage/open-book.svg';
 import interviewIcon from '@images/mypage/conversation-icon.svg';
 
-export const statusTag = (moim: IMoim) => {
-  if (moim.status === 'RECRUIT') {
-    return '모집중';
-  } else if (moim.status === 'PROGRESS') {
-    return '진행중';
-  } else if (moim.status === 'END') {
+export const statusTag = (moim?: IMoim) => {
+  if (moim?.status === 'END') {
     return '종료';
+  } else if (moim?.maxParticipants === moim?.participants) {
+    return '모집완료';
+  } else if (moim?.isConfirmed === true) {
+    return '개설확정';
+  } else if (moim?.isConfirmed === false) {
+    return '모집중';
   }
 };
 
@@ -24,22 +26,22 @@ export const reviewRateTag = (review: IReview) => {
   }
 };
 
-export const moimTypeTag = (moim: IMoim) => {
-  if (moim.moimType === 'PROJECT') {
+export const moimTypeTag = (moim?: IMoim) => {
+  if (moim?.moimType === 'PROJECT') {
     return '프로젝트';
-  } else if (moim.moimType === 'STUDY') {
+  } else if (moim?.moimType === 'STUDY') {
     return '스터디';
-  } else if (moim.moimType === 'INTERVIEW') {
+  } else if (moim?.moimType === 'INTERVIEW') {
     return '면접';
   }
 };
 
-export const moimTypeIcon = (moim: IMoim) => {
-  if (moim.moimType === 'PROJECT') {
+export const moimTypeIcon = (moim?: IMoim) => {
+  if (moim?.moimType === 'PROJECT') {
     return projectIcon;
-  } else if (moim.moimType === 'STUDY') {
+  } else if (moim?.moimType === 'STUDY') {
     return studyIcon;
-  } else if (moim.moimType === 'INTERVIEW') {
+  } else if (moim?.moimType === 'INTERVIEW') {
     return interviewIcon;
   }
 };
