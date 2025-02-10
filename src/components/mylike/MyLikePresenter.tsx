179740015b444186f5a267967619f8cc69094be6
doy00 <dhkm201@gmail.com>
2025-02-10 14:43:17 +0900

    feat: 찜페이지 로딩 container로 이동 style: toast, skeleton 스타일 수정

--- src/components/mylike/MyLikePresenter.tsx
+++ src/components/mylike/MyLikePresenter.tsx
@@ -13,15 +13,15 @@ interface IMyLikePresenter {
 
 export default function MyLikePresenter ({ moims, onRemoveLike, onClickCard }: IMyLikePresenter
   ) {
-    if (moims.length === 0) {
-      return (
-        <div className="w-full min-h-screen mx-auto px-4 bg-background200 xs:max-w-screen-xs sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg">
-          <Header />
-          <div className="pt-[14px] relative flex flex-col items-center"><DuduEmpty /></div>
-          <div className="text-center text-gray600 text-caption-normal">찜한 모임이 없습니다.</div>
-        </div>
-      );
-    }
+    // if (moims.length === 0) {
+    //   return (
+    //     <div className="w-full min-h-screen mx-auto px-4 md:px-20 bg-background200 xs:max-w-screen-xs sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg">
+    //       <Header />
+    //       <div className="pt-[14px] relative flex flex-col items-center"><DuduEmpty /></div>
+    //       <div className="text-center text-gray600 text-caption-normal">찜한 모임이 없습니다.</div>
+    //     </div>
+    //   );
+    // }
 
   return (
     <div className="w-full min-h-screen mx-auto px-4 pb-[92px] md:px-20 bg-background200 xs:max-w-screen-xs sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg"
