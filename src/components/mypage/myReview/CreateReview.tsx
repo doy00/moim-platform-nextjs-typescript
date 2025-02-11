d2e58f84f43a45b4bcd1062533af9b62e0755ada
ayoooyh <syh9566@naver.com>
2025-02-11 16:35:00 +0900

    remove : 필요없는 코드 삭제

--- src/components/mypage/myReview/CreateReview.tsx
+++ src/components/mypage/myReview/CreateReview.tsx
@@ -113,137 +113,132 @@ export default function CreateReview() {
     return <div>오류가 발생했습니다</div>;
   }
 
-  if (isParticipatedUser) {
-    return (
-      <div className="flex flex-col gap-10 p-4">
-        <div className="flex flex-col gap-6">
-          <div className="flex items-center justify-between">
-            <span className="text-body-1-normal font-semibold text-gray800">리뷰 작성하기</span>
+  return (
+    <div className="flex flex-col gap-10 p-4">
+      <div className="flex flex-col gap-6">
+        <div className="flex items-center justify-between">
+          <span className="text-body-1-normal font-semibold text-gray800">리뷰 작성하기</span>
+          <Image
+            src="/images/mypage/close.svg"
+            alt="close"
+            width={24}
+            height={24}
+            onClick={handleClose}
+            className="cursor-pointer"
+          />
+        </div>
+        <span className="text-heading1 text-gray800 font-semibold">
+          {nickname}님<br /> 이번 모임에 대한 리뷰를 작성해주세요.
+        </span>
+      </div>
+      <div>
+        {data?.map((moim) => (
+          <GatheringCard
+            moim={moim}
+            key={moim.moimId}
+            hideStatus={true}
+            hideReviewButton={true}
+            disableLink={true}
+          />
+        ))}
+      </div>
+
+      <form className="flex flex-col gap-6" onSubmit={methods.handleSubmit(onSubmit)}>
+        <span>모임은 어땠나요?</span>
+
+        <div className="flex gap-7 items-center justify-center sm:justify-start">
+          <button type="button" onClick={() => handleReview('SOSO')}>
             <Image
-              src="/images/mypage/close.svg"
-              alt="close"
-              width={24}
-              height={24}
-              onClick={handleClose}
-              className="cursor-pointer"
+              src={
+                clicked === 'SOSO'
+                  ? '/images/mypage/dude-grade-bad-on.svg'
+                  : '/images/mypage/dude-grade-bad-off.svg'
+              }
+              alt="그냥그래요"
+              width={80}
+              height={112}
             />
-          </div>
-          <span className="text-heading1 text-gray800 font-semibold">
-            {nickname}님<br /> 이번 모임에 대한 리뷰를 작성해주세요.
-          </span>
-        </div>
-        <div>
-          {data?.map((moim) => (
-            <GatheringCard
-              moim={moim}
-              key={moim.moimId}
-              hideStatus={true}
-              hideReviewButton={true}
-              disableLink={true}
-            />
-          ))}
-        </div>
-
-        <form className="flex flex-col gap-6" onSubmit={methods.handleSubmit(onSubmit)}>
-          <span>모임은 어땠나요?</span>
-
-          <div className="flex gap-7 items-center justify-center sm:justify-start">
-            <button type="button" onClick={() => handleReview('SOSO')}>
-              <Image
-                src={
-                  clicked === 'SOSO'
-                    ? '/images/mypage/dude-grade-bad-on.svg'
-                    : '/images/mypage/dude-grade-bad-off.svg'
-                }
-                alt="그냥그래요"
-                width={80}
-                height={112}
-              />
-            </button>
-            <button type="button" onClick={() => handleReview('GOOD')}>
-              <Image
-                src={
-                  clicked === 'GOOD'
-                    ? '/images/mypage/dude-grade-good-on.svg'
-                    : '/images/mypage/dude-grade-good-off.svg'
-                }
-                alt="괜찮아요"
-                width={80}
-                height={112}
-              />
-            </button>
-            <button type="button" onClick={() => handleReview('RECOMMEND')}>
-              <Image
-                src={
-                  clicked === 'RECOMMEND'
-                    ? '/images/mypage/dude-grade-best-on.svg'
-                    : '/images/mypage/dude-grade-best-off.svg'
-                }
-                alt="추천해요"
-                width={80}
-                height={112}
-              />
-            </button>
-          </div>
-          {submitError && <div className="text-red-500 text-sm mt-2">{submitError}</div>}
-
-          <div className="flex flex-col gap-6">
-            <label htmlFor="review" className="text-body-2-nomal font-medium text-gray-800">
-              구체적인 경험을 알려주세요
-            </label>
-            <textarea
-              id="review"
-              value={review}
-              onChange={(e) => {
-                setReview(e.target.value);
-                methods.setValue('review', e.target.value);
-              }}
-              className="w-full h-40 rounded-xl px-4 py-[18px] bg-background400 resize-none"
-              placeholder="모임의 장소, 환경, 진행, 구성 등 만족스러웠나요?"
-            />
-          </div>
-
-          <button
-            type="submit"
-            disabled={isDisabled}
-            className={`w-full h-14 rounded-2xl py-[17px] text-body-1-nomal font-semibold
-              ${isFormValid ? 'bg-orange200 text-white' : 'bg-gray950 text-gray600'}`}
-          >
-            작성완료
           </button>
-        </form>
+          <button type="button" onClick={() => handleReview('GOOD')}>
+            <Image
+              src={
+                clicked === 'GOOD'
+                  ? '/images/mypage/dude-grade-good-on.svg'
+                  : '/images/mypage/dude-grade-good-off.svg'
+              }
+              alt="괜찮아요"
+              width={80}
+              height={112}
+            />
+          </button>
+          <button type="button" onClick={() => handleReview('RECOMMEND')}>
+            <Image
+              src={
+                clicked === 'RECOMMEND'
+                  ? '/images/mypage/dude-grade-best-on.svg'
+                  : '/images/mypage/dude-grade-best-off.svg'
+              }
+              alt="추천해요"
+              width={80}
+              height={112}
+            />
+          </button>
+        </div>
+        {submitError && <div className="text-red-500 text-sm mt-2">{submitError}</div>}
 
-        {showModal && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-            <div className="flex flex-col gap-6 bg-background400 pt-8 pb-6 px-6 rounded-3xl w-full max-w-[320px]">
-              <div className="flex flex-col gap-3 items-center justify-center">
-                <p className="text-body-1-nomal font-semibold text-gray800">취소하고 나갈까요?</p>
-                <p className="text-body-2-nomal font-medium text-gray400">
-                  나가면 내용은 저장되지 않아요.
-                </p>
-              </div>
+        <div className="flex flex-col gap-6">
+          <label htmlFor="review" className="text-body-2-nomal font-medium text-gray-800">
+            구체적인 경험을 알려주세요
+          </label>
+          <textarea
+            id="review"
+            value={review}
+            onChange={(e) => {
+              setReview(e.target.value);
+              methods.setValue('review', e.target.value);
+            }}
+            className="w-full h-40 rounded-xl px-4 py-[18px] bg-background400 resize-none outline-none"
+            placeholder="모임의 장소, 환경, 진행, 구성 등 만족스러웠나요?"
+          />
+        </div>
 
-              <div className="flex gap-4">
-                <button
-                  className="flex-1 px-[12.5px] py-3.5 rounded-[14px] bg-gray200 text-gray500 text-body-2-nomal font-semibold"
-                  onClick={() => router.replace('/mypage')}
-                >
-                  나가기
-                </button>
-                <button
-                  className="flex-1 px-[12.5px] py-3.5 rounded-[14px] bg-gray800 text-gray100 text-body-2-nomal font-semibold"
-                  onClick={() => setShowModal(false)}
-                >
-                  이어서 작성하기
-                </button>
-              </div>
+        <button
+          type="submit"
+          disabled={isDisabled}
+          className={`w-full h-14 rounded-2xl py-[17px] text-body-1-nomal font-semibold
+              ${isFormValid ? 'bg-orange200 text-white' : 'bg-gray950 text-gray600'} outline-none`}
+        >
+          작성완료
+        </button>
+      </form>
+
+      {showModal && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <div className="flex flex-col gap-6 bg-background400 pt-8 pb-6 px-6 rounded-3xl w-full max-w-[320px]">
+            <div className="flex flex-col gap-3 items-center justify-center">
+              <p className="text-body-1-nomal font-semibold text-gray800">취소하고 나갈까요?</p>
+              <p className="text-body-2-nomal font-medium text-gray400">
+                나가면 내용은 저장되지 않아요.
+              </p>
+            </div>
+
+            <div className="flex gap-4">
+              <button
+                className="flex-1 px-[12.5px] py-3.5 rounded-[14px] bg-gray200 text-gray500 text-body-2-nomal font-semibold"
+                onClick={() => router.replace('/mypage')}
+              >
+                나가기
+              </button>
+              <button
+                className="flex-1 px-[12.5px] py-3.5 rounded-[14px] bg-gray800 text-gray100 text-body-2-nomal font-semibold"
+                onClick={() => setShowModal(false)}
+              >
+                이어서 작성하기
+              </button>
             </div>
           </div>
-        )}
-      </div>
-    );
-  } else {
-    // TODO : 모달이나 페이지 생성 예정
-    return <div>이 모임의 참여자가 아닙니다.</div>;
-  }
+        </div>
+      )}
+    </div>
+  );
 }
