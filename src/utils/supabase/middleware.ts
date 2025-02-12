7139ea19a5b11e7cf3a75c2eae0761badba1309e
Eun Oh <bdohhhhh@gmail.com>
2025-02-12 20:08:41 +0900

    chore: getAll

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -38,9 +38,13 @@ export const updateSession = async (request: NextRequest) => {
   } = await supabase.auth.getUser();
 
   console.log('user in middlware ====>', user);
-  console.log('cookieStore in middlware ====>', cookieStore);
+  console.log('cookieStore in middlware ====>', cookieStore.getAll());
   // 로그인이 안되었을 때, /, /api, /detail, /auth 를 제외하고는 모두 로그인 페이지로 리다이렉트 한다.
   // /favorite, /make, /mypage 는 로그인이 필요하므로 리다이렉트 한다
+
+  // path 가 mypage 인 경우 콘솔
+  // 그리고 쿠키도 찍어볼 것
+  // 쿠키 보안단계를 낮춰볼 것
   if (
     !user &&
     request.nextUrl.pathname !== '/' &&
