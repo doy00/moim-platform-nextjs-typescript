0fe1404d874f614fdd214b10762aa4cd8e654138
doy00 <dhkm201@gmail.com>
2025-02-12 18:28:44 +0900

    feat: prefetch 훅 분리

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,33 +1,19 @@
 // app/detail/[id]/page.tsx
-import { getDetail } from '@/apis/detail/detail.api';
 import DetailContainer from '@/containers/detail/DetailContainer';
-import { QueryClient } from '@tanstack/react-query';
 import { Suspense } from "react";
-import { IMoimMasterResponse } from '@/types/detail/t-moim';
 import { DetailSkeleton } from '@/components/detail/DetailSkeleton';
-import { QUERY_KEYS } from '@/constants/detail/detail.const';
+import { usePrefetchDetail } from '@/hooks/detail/useMoimDetail';
 
 interface DetailPageProps {
   params: Promise<{ id: string }>;
 }
 
 export default async function DetailPage({ params } : DetailPageProps) {
-  const queryClient = new QueryClient();
-
   // Dynamic route parameters 사용을 위해 비동기 처리
   // const { id: moimId } = await Promise.resolve(params);
   const { id: moimId } = await params;
 
-  // 서버에서 초기 데이터 prefetching
-  try {
-    await 
-    queryClient.prefetchQuery<IMoimMasterResponse>({
-      queryKey: QUERY_KEYS.MOIM_DETAIL(moimId),
-      queryFn: () => getDetail(moimId),
-    })
-  } catch (error) {
-    console.error('Prefetch 에러 발생:', error);
-  }
+  usePrefetchDetail(moimId);
 
   return (
     <div>
--- src/hooks/detail/useMoimDetail.ts
+++ src/hooks/detail/useMoimDetail.ts
@@ -1,7 +1,7 @@
 // 모임 상세 데이터 조회 훅
 import { getDetail } from '@/apis/detail/detail.api';
 import { IMoimMasterResponse } from '@/types/detail/t-moim';
-import { useQuery } from '@tanstack/react-query';
+import { QueryClient, useQuery } from '@tanstack/react-query';
 import { QUERY_KEYS } from '@/constants/detail/detail.const';
 import { TMe } from '@/types/auth/auth.type';
 
@@ -17,3 +17,16 @@ export const useMoimDetail = (moimId: string, options: UseMoimDetailOptions = {}
     enabled: options.enabled,
   });
 };
+
+// 서버에서 초기 데이터 prefetching
+export function usePrefetchDetail(moimId: string): void {
+  const queryClient = new QueryClient();
+  try {
+    queryClient.prefetchQuery<IMoimMasterResponse>({
+      queryKey: QUERY_KEYS.MOIM_DETAIL(moimId),
+      queryFn: () => getDetail(moimId),
+    })
+  } catch (error) {
+    console.error('Prefetch 에러 발생:', error);
+  }
+}
